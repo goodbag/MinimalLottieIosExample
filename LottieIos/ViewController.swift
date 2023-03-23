802f9c0035d3ce8b7d2a65d532cddaa12c90e268
Johannes Herrnegger <johannes.herrnegger@gmail.com>
2023-03-23 14:57:10 +0100

    initial commit

--- LottieIos/ViewController.swift
+++ LottieIos/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    private var animationView: LottieAnimationView?
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+      super.viewDidLoad()
+      
+      // 2. Start LottieAnimationView with animation name (without extension)
+      animationView = .init(name: "earth_loading", subdirectory: "Animations")
+      animationView!.frame = view.bounds
+      
+      // 3. Set animation content mode
+      animationView!.contentMode = .scaleAspectFit
+      
+      // 4. Set animation loop mode
+      animationView!.loopMode = .loop
+      
+      // 5. Adjust animation speed
+      animationView!.animationSpeed = 1
+      view.addSubview(animationView!)
+      
+      // 6. Play animation
+      
+      animationView!.play()
+      
     }
 
 
